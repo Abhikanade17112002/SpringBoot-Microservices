18b3515f1ff8f320c480ed760fc5d01685c82306
Abhishek Kanade <abhishek.kanade.dev@gmail.com>
2026-05-24 10:44:48 +0530

    Added The RestTemplate For Fetching ratings For User

--- UserService/UserService/src/main/java/com/microsercives/userservice/configurations/ApplicationConfigurations.java
+++ UserService/UserService/src/main/java/com/microsercives/userservice/configurations/ApplicationConfigurations.java
@@ -5,6 +5,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationConfigurations {
@@ -18,5 +19,10 @@ public class ApplicationConfigurations {
                 .setMatchingStrategy(MatchingStrategies.STRICT);
 
         return modelMapper;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- UserService/UserService/src/main/java/com/microsercives/userservice/controllers/UserController.java
+++ UserService/UserService/src/main/java/com/microsercives/userservice/controllers/UserController.java
@@ -1,21 +1,28 @@
 package com.microsercives.userservice.controllers;
 import com.microsercives.userservice.dtos.CreateUserRequestDTO;
+import com.microsercives.userservice.entities.Rating;
 import com.microsercives.userservice.entities.User;
 import com.microsercives.userservice.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-
+    private static final String RATING_SERVICE_URL = "http://localhost:8082/api/v1/ratings/users/";
     private final UserService userService;
+    private final RestTemplate restTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, RestTemplate restTemplate) {
         this.userService = userService;
+        this.restTemplate = restTemplate;
     }
 
     // CREATE USER
@@ -34,6 +41,11 @@ public class UserController {
             @PathVariable String userId) {
 
         User user = userService.getUserById(userId);
+
+
+        List<Rating> userRatings = restTemplate.getForObject(RATING_SERVICE_URL + userId, List.class);
+
+        user.setRatings(userRatings);
 
         return ResponseEntity.ok(user);
     }
