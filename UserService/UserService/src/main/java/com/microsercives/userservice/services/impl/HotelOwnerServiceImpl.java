d8203b530e7be6255ea41a0bd4130497401bb13b
AbhiKanade17112002 <abhishekrangnathkanade21@gmail.com>
2026-06-04 22:19:12 +0530

    Latest

--- UserService/UserService/src/main/java/com/microsercives/userservice/services/impl/HotelOwnerServiceImpl.java
+++ UserService/UserService/src/main/java/com/microsercives/userservice/services/impl/HotelOwnerServiceImpl.java
@@ -12,7 +12,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-
+import org.springframework.stereotype.Service;
+@Service
 public class HotelOwnerServiceImpl implements HotelOwnerService {
     @Autowired
     private ModelMapper modelMapper ;
