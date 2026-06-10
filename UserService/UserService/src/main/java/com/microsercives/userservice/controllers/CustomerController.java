ef82b10553f0a70f325d45a5d6c79a88d3513998
Abhishek Kanade <abhishek.kanade.dev@gmail.com>
2026-06-11 00:04:19 +0530

    Added Controller Layer test Cases

--- UserService/UserService/src/main/java/com/microsercives/userservice/controllers/CustomerController.java
+++ UserService/UserService/src/main/java/com/microsercives/userservice/controllers/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,6 +40,7 @@ public class CustomerController {
                 .body(customerSevice.getRegisteredCustomerById(customerId));
     }
 
+    @PreAuthorize("hasAuthority('ROLE_CUSTOMER')")
     @PutMapping("/{customerId}/activate")
     public ResponseEntity<CustomerResponseDTO> activateCustomerById(@PathVariable( name = "customerId") String customerId){
         return ResponseEntity
